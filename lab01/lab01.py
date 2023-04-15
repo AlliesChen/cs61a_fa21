a857b9800b5b70da2d8ae210b27b6ff4962629d8
YPChen <cyphuaenng@pm.me>
2023-04-15 18:47:41 +0800

    initial commit

---
+++ lab01/lab01.py
@@ -0,0 +1,67 @@
+def falling(n, k):
+    """Compute the falling factorial of n to depth k.
+
+    >>> falling(6, 3)  # 6 * 5 * 4
+    120
+    >>> falling(4, 3)  # 4 * 3 * 2
+    24
+    >>> falling(4, 1)  # 4
+    4
+    >>> falling(4, 0)
+    1
+    """
+    "*** YOUR CODE HERE ***"
+    accum = 1;
+    counter = k - 1;
+    while counter >= 0:
+        accum = accum * (n - counter); 
+        counter -= 1;
+    return accum;
+
+
+
+def sum_digits(y):
+    """Sum all the digits of y.
+
+    >>> sum_digits(10) # 1 + 0 = 1
+    1
+    >>> sum_digits(4224) # 4 + 2 + 2 + 4 = 12
+    12
+    >>> sum_digits(1234567890)
+    45
+    >>> a = sum_digits(123) # make sure that you are using return rather than print
+    >>> a
+    6
+    """
+    "*** YOUR CODE HERE ***"
+    result = 0
+    for c in str(y):
+        result += int(c)
+    return result
+
+
+def double_eights(n):
+    """Return true if n has two eights in a row.
+    >>> double_eights(8)
+    False
+    >>> double_eights(88)
+    True
+    >>> double_eights(2882)
+    True
+    >>> double_eights(880088)
+    True
+    >>> double_eights(12345)
+    False
+    >>> double_eights(80808080)
+    False
+    """
+    "*** YOUR CODE HERE ***"
+    slow = 0;
+    for c in str(n):
+        if c == '8':
+            slow += 1
+        else:
+            slow = 0
+        if slow == 2:
+            return True
+    return False
