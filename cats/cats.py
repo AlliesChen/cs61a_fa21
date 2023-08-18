ced8b8c152426093a41c77d33a46e3071a655eee
ted <tedchen524@gmail.com>
2023-08-18 08:16:08 +0800

    :sparkles: Update project2 - cats
    Add problem 5

--- cats/cats.py
+++ cats/cats.py
@@ -123,7 +123,7 @@ def wpm(typed: str, elapsed: int) -> float:
 # Phase 2 #
 ###########
 
-def autocorrect(typed_word, valid_words, diff_function, limit):
+def autocorrect(typed_word: str, valid_words: list[str], diff_function: Callable[[str, str, int], int], limit: int) -> str:
     """Returns the element of VALID_WORDS that has the smallest difference
     from TYPED_WORD. Instead returns TYPED_WORD if that difference is greater
     than LIMIT.
@@ -142,7 +142,20 @@ def autocorrect(typed_word, valid_words, diff_function, limit):
     'testing'
     """
     # BEGIN PROBLEM 5
-    "*** YOUR CODE HERE ***"
+    lowest_diff = 999
+    lowest_diff_idx: int | None = None
+    for idx in range(len(valid_words)):
+        if typed_word == valid_words[idx] or limit == 0:
+            return typed_word
+        if lowest_diff_idx == None:
+            lowest_diff_idx = idx
+            lowest_diff = diff_function(typed_word, valid_words[idx], limit)
+        else:
+            lowest_diff_idx = idx if diff_function(
+                typed_word, valid_words[idx], limit) < lowest_diff else lowest_diff_idx
+            lowest_diff = diff_function(
+                typed_word, valid_words[lowest_diff_idx], limit)
+    return valid_words[lowest_diff_idx] if diff_function(typed_word, valid_words[lowest_diff_idx], limit) < limit else typed_word
     # END PROBLEM 5
 
 
